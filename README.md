40e899dac1ba0e2c34911f3f73b727a382eb0ef7
evanstanderwick <evanstanderwick@gmail.com>
2021-04-05 16:55:58 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+OSU SP21 Ling 3802 Final Project
+Phishing Email Feature Design
---
+++ dataParser.py
@@ -0,0 +1,27 @@
+# Author: Evan Standerwick
+# This program takes an input data set (TODO figure out data set file format) and outputs a tsv to be fed into the Stanford classifier.
+# Developed on Windows 10 using Python 3.8.1
+# Instructions: run using ./python dataParser.py <Input_Data_File>.(TODO figure out data set file type) <Output_File_Name>.tsv
+
+
+def main():
+    # 1. OPEN UP INPUT DATA FILE
+    
+    # 2. BEGIN PARSING
+
+    # 3. GET ACTUAL CATEGORY AND BINARY FEATURES FOR CURRENT ENTRY
+
+    # 4. OUTPUT CURRENT ENTRY DATA TO OUTPUT .TSV FILE
+
+    # 5. REPEAT STEPS 3-4 UNTIL END OF FILE
+
+    # 6. CLOSE IO STREAMS AND EXIT
+
+    print("main")
+
+
+
+
+
+if __name__ == '__main__':
+    main()
